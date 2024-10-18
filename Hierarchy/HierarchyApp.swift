a1afeebae647808c70dee4cb0bf8407f7ac8ad6c
LukasSkrivanek <luky.skrivos@gmail.com>
2024-10-18 13:55:25 +0200

    Initial Commit

---
+++ Hierarchy/HierarchyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HierarchyApp.swift
+//  Hierarchy
+//
+//  Created by macbook on 18.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct HierarchyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
